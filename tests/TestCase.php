acdff63920f9be27bedfd5359104042e9c39f3fb
R. Aravindan <aravind.ram@ranium.in>
2024-09-10 10:21:41 +0530

    separate config file for test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -66,104 +66,10 @@ class TestCase extends Orchestra
 
     protected function setupConfig()
     {
-        
-       $configs = [
-            'cache' => [
-                'store' => null,
-                'prefix' => '2fa.code',
-            ],
-            'recovery' => [
-                'enabled' => true,
-                'codes' => 10,
-                'length' => 8,
-            ],
-            'safe_devices' => [
-                'enabled' => true,
-                'cookie' => '_2fa_remember',
-                'max_devices' => 3,
-                'expiration_days' => 14,
-            ],
-            'confirm' => [
-                'key' => '_2fa',
-                'time' => 60 * 3, // 3 hours
-            ],
-            'login' => [
-                'view' => 'two-factor::login',
-                'key' => '_2fa_login',
-                'flash' => true,
-            ],
-            'secret_length' => 20,
-            'issuer' => env('OTP_TOTP_ISSUER'),
-
-            'totp' => [
-                'digits' => 6,
-                'seconds' => 30,
-                'window' => 1,
-                'algorithm' => 'sha1',
-            ],
-            'qr_code' => [
-                'size' => 400,
-                'margin' => 4,
-            ]
-       ];
-
+        $userMigration = include __DIR__.'/config.php';
         config()->set('database.default', 'testing');
-
-        config()->set('two-factor', $configs);
-
-        config()->set('auth', [
-
-            'defaults' => [
-                'guard' => 'web',
-                'passwords' => 'users',
-            ],
-
-            'guards' => [
-                
-                'web' => [
-                    'driver' => 'session',
-                    'provider' => 'users',
-                ],
-            ],
-        
-            'providers' => [
-                'users' => [
-                    'driver' => 'eloquent',
-                    'model' => User::class,
-                ]
-            ],
-        
-            'passwords' => [
-                'users' => [
-                    'provider' => 'users',
-                    'table' => 'password_reset_tokens',
-                    'expire' => 60,
-                    'throttle' => 60,
-                ],
-            ],
-            'password_timeout' => 10800,
-        ]);
-
-        config()->set('session', [
-            'driver' => env('SESSION_DRIVER', 'file'),
-            'lifetime' => env('SESSION_LIFETIME', 120),
-            'expire_on_close' => false,
-            'encrypt' => false,
-            'files' => storage_path('framework/sessions'),
-            'connection' => env('SESSION_CONNECTION'),
-            'table' => 'sessions',
-            'store' => env('SESSION_STORE'),
-            'lottery' => [2, 100],
-            'cookie' => env(
-                'SESSION_COOKIE',
-                Str::slug(env('APP_ENV', 'laravel'), '_').'_session'
-            ),
-            'path' => '/',            
-            'domain' => env('SESSION_DOMAIN'),        
-            'secure' => env('SESSION_SECURE_COOKIE',true),            
-            'http_only' => true,            
-            'same_site' => 'lax',            
-        ]);
+        config()->set('two-factor', $userMigration['two-factor']);
+        config()->set('auth', $userMigration['auth']);
     }
 
     public function createUser() 
--- tests/config.php
+++ tests/config.php
@@ -1,41 +1,76 @@
 <?php
+
+use Optimacloud\Filament2fa\Tests\Models\User;
+
 return [
+    'two-factor' => [
+        'cache' => [
+            'store' => null,
+            'prefix' => '2fa.code',
+        ],
+        'recovery' => [
+            'enabled' => true,
+            'codes' => 10,
+            'length' => 8,
+        ],
+        'safe_devices' => [
+            'enabled' => true,
+            'cookie' => '_2fa_remember',
+            'max_devices' => 3,
+            'expiration_days' => 14,
+        ],
+        'confirm' => [
+            'key' => '_2fa',
+            'time' => 60 * 3, // 3 hours
+        ],
+        'login' => [
+            'view' => 'two-factor::login',
+            'key' => '_2fa_login',
+            'flash' => true,
+        ],
+        'secret_length' => 20,
+        'issuer' => env('OTP_TOTP_ISSUER'),
 
-    'cache' => [
-        'store' => null,
-        'prefix' => '2fa.code',
+        'totp' => [
+            'digits' => 6,
+            'seconds' => 30,
+            'window' => 1,
+            'algorithm' => 'sha1',
+        ],
+        'qr_code' => [
+            'size' => 400,
+            'margin' => 4,
+        ]
     ],
-    'recovery' => [
-        'enabled' => true,
-        'codes' => 10,
-        'length' => 8,
-    ],
-    'safe_devices' => [
-        'enabled' => false,
-        'cookie' => '_2fa_remember',
-        'max_devices' => 3,
-        'expiration_days' => 14,
-    ],
-    'confirm' => [
-        'key' => '_2fa',
-        'time' => 60 * 3, // 3 hours
-    ],
-    'login' => [
-        'view' => 'two-factor::login',
-        'key' => '_2fa_login',
-        'flash' => true,
-    ],
-    'secret_length' => 20,
-    'issuer' => env('OTP_TOTP_ISSUER'),
+    'auth' => [
+        'defaults' => [
+            'guard' => 'web',
+            'passwords' => 'users',
+        ],
 
-    'totp' => [
-        'digits' => 6,
-        'seconds' => 30,
-        'window' => 1,
-        'algorithm' => 'sha1',
-    ],
-    'qr_code' => [
-        'size' => 400,
-        'margin' => 4,
+        'guards' => [
+            
+            'web' => [
+                'driver' => 'session',
+                'provider' => 'users',
+            ],
+        ],
+
+        'providers' => [
+            'users' => [
+                'driver' => 'eloquent',
+                'model' => User::class,
+            ]
+        ],
+
+        'passwords' => [
+            'users' => [
+                'provider' => 'users',
+                'table' => 'password_reset_tokens',
+                'expire' => 60,
+                'throttle' => 60,
+            ],
+        ],
+        'password_timeout' => 10800,
     ]
 ];
