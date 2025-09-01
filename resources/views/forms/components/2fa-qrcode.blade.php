44580669a461f32396ce69ddf296f72aeb24fc7b
Lee Evans <lee@ekouk.com>
2025-09-01 10:34:59 +0200

    Wrap component in a div

--- resources/views/forms/components/2fa-qrcode.blade.php
+++ resources/views/forms/components/2fa-qrcode.blade.php
@@ -1,12 +1,13 @@
 @php
     $twoFa = $this->prepareTwoFactor();
 @endphp
+<div>
+    <div class="flex items-center justify-center">
+        {!! $twoFa['qr'] !!}
+    </div>
 
-<div class="flex items-center justify-center">
-    {!! $twoFa['qr'] !!}
+    <p class="mt-3 text-xs text-gray-500">
+        {{ __('If you can’t scan the QR, add this account manually using the secret:') }}
+        <code class="font-mono">{{ $twoFa['secret'] }}</code>
+    </p>
 </div>
-
-<p class="mt-3 text-xs text-gray-500">
-    {{ __('If you can’t scan the QR, add this account manually using the secret:') }}
-    <code class="font-mono">{{ $twoFa['secret'] }}</code>
-</p>
